a7c7f1b77d98fb1bffb9bf94b30ef077a063397c
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-05-03 11:31:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,17 @@
 // Your JS code here.
-  // Get all the images that need to slide in on scroll
-  const slideInImages = document.querySelectorAll('.slide-in');
+function checkSlide(e) {
+	const images = document.querySelectorAll(".slide-in");
 
-  // Function to add the active classname to the image, when scrolled to it
-  function checkSlide() {
-    // Loop over each image
-    slideInImages.forEach(slideInImage => {
-      // Get the bottom of the image
-      const slideInImageBottom = slideInImage.offsetTop + slideInImage.height;
-      // Check if the image is in the viewport
-      const isHalfShown = window.scrollY + window.innerHeight > slideInImage.offsetTop + slideInImage.height / 2;
-      const isNotScrolledPast = window.scrollY < slideInImageBottom;
-      if (isHalfShown && isNotScrolledPast) {
-        slideInImage.classList.add('active');
-      } else {
-        slideInImage.classList.remove('active');
-      }
-    });
-  }
-
-  // Debounce function to limit the number of times the checkSlide function is called
-  function debounce(func, wait = 20, immediate = true) {
-    let timeout;
-    return function() {
-      const context = this, args = arguments;
-      const later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-  }
-
-  // Listen to the scroll event and call the checkSlide function
-  window.addEventListener('scroll', debounce(checkSlide));
+	images.forEach((image) => {
+	const slideInAt =
+	window.scrollY + window.innerHeight - image.height / 2;
+	const imageBottom = image.offsetTop + image.height;
+	const isHalfShown = slideInAt > image.offsetTop;
+	const isNotScrolledPast = window.scrollY < imageBottom;
+	if (isHalfShown && isNotScrolledPast) {
+	image.classList.add("active");
+	} else {
+	image.classList.remove("active");
+	}
+	});
+	}
